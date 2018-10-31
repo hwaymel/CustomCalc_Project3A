20769031ee746c3407deae46a04985bd493af54a
hwaymel <42789557+hwaymel@users.noreply.github.com>
2018-10-30 18:03:31 -0600

    Add files via upload

--- AreaVolCalc_3A/frmAreaVolCalc.cs
+++ AreaVolCalc_3A/frmAreaVolCalc.cs
@@ -34,6 +34,7 @@ namespace AreaVolCalc_3A
                     txtHeight.Visible = true;
                     lblHeight.Visible = true;
                     txtTotal.Text = "";
+                    txtDivTotal.Text = "";
                 }
             }
 
@@ -50,6 +51,7 @@ namespace AreaVolCalc_3A
                     txtHeight.Visible = false;
                     lblHeight.Visible = false;
                     txtTotal.Text = "";
+                    txtDivTotal.Text = "";
                 }
             }
 
@@ -66,32 +68,78 @@ namespace AreaVolCalc_3A
                     txtHeight.Visible = false;
                     lblHeight.Visible = false;
                     txtTotal.Text = "";
-                }
+                    txtDivTotal.Text = "";
+            }
             }
 
             private void btnCalculate_Click(object sender, EventArgs e)
             {
-                double length = Convert.ToDouble(txtLength.Text);
-                double width = Convert.ToDouble(txtWidth.Text);
-                double height = Convert.ToDouble(txtHeight.Text);
-                double total = 0d;
+                try
+                {
+                    int length = Convert.ToInt32(txtLength.Text);
+                    int width = Convert.ToInt32(txtWidth.Text);
+                    int height = Convert.ToInt32(txtHeight.Text);
+                    int divisor = Convert.ToInt32(txtDivBy.Text);
+                    string strDivisor = txtDivBy.Text;
+                    int divLength = 0;
+                    double total = 0d;
 
+                    if (rdArea.Checked)
+                        total = length * width;
+                    else if (rdVolume.Checked)
+                        total = length * width * height;
+                    else
+                        total = Math.PI * Math.Pow(length, 2);
 
-                if (rdArea.Checked)
-                    total = length * width;
-                else if (rdVolume.Checked)
-                    total = length * width * height;
-                else
-                    total = Math.PI * Math.Pow(length, 2);
+                    //Custom Exception Check - Negative 
+                    if (strDivisor.StartsWith("9"))
+                    {
+                    throw new Exception("Div By value cannot start with the number 9; please enter a different number.");   
+                    }
+                    //Additional Division Problem as part of 3A requirements
+                    divLength = length / divisor;
 
-                if (rdArea.Checked)
-                    txtTotal.Text = Convert.ToString(total) + " sq. ft.";
-                else if (rdVolume.Checked)
+                    if (rdArea.Checked)
+                    {
+                        txtTotal.Text = Convert.ToString(total) + " sq. ft.";
+                        txtDivTotal.Text = Convert.ToString(divLength);
+                    }
+                    else if (rdVolume.Checked)
+                    { 
                     txtTotal.Text = Convert.ToString(total) + " cubic ft.";
-                else
-                    txtTotal.Text = Convert.ToString(total);
+                    txtDivTotal.Text = Convert.ToString(divLength);
+                    }
+                    else
+                        txtTotal.Text = Convert.ToString(total);
+                    }
 
-            }
+                //Exception handling section - Added to complete Project 3A
+                catch(FormatException)
+                {
+                MessageBox.Show(
+                    "Invalid numeric format. Please check all entries and make sure you " +
+                    "have entered positive integer values that are < 2147483648.",
+                    "Entry Format Error");
+                 }
+                catch(OverflowException)
+                {
+                MessageBox.Show(
+                    "Overflow Error; please enter smaller numeric values (must be < 2147483648).",
+                    "Overflow Error");
+                }
+                catch (DivideByZeroException)
+                {
+                MessageBox.Show(
+                    "Divide by Zero Error; please a number other than zero in the Div By field.",
+                    "Divide by Zero Error");
+                }
+                catch (Exception ex)
+                {
+                MessageBox.Show(
+                    ex.Message,
+                    "Div By Begins with 9 Error");
+                }
+        }
 
             private void btnClearInputs_Click(object sender, EventArgs e)
             {
@@ -99,6 +147,7 @@ namespace AreaVolCalc_3A
                 txtWidth.Text = "0";
                 txtHeight.Text = "0";
                 txtTotal.Text = "";
+                txtDivTotal.Text = "";
             }
 
             private void btnExit_Click(object sender, EventArgs e)
